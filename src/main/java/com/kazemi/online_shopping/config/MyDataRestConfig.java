a9d3586ef9686582fe02475827e9bb11626c2856
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-03-03 16:51:58 +0330

    Add a Configuration Class to Configure Repository REST

---
+++ src/main/java/com/kazemi/online_shopping/config/MyDataRestConfig.java
@@ -0,0 +1,34 @@
+package com.kazemi.online_shopping.config;
+
+import com.kazemi.online_shopping.entity.Product;
+import com.kazemi.online_shopping.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+/**
+ * @author fh.kazemi
+ **/
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        //disable Http method for Products: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //disable Http method for ProductCategory: PUT, POST, DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
+}
