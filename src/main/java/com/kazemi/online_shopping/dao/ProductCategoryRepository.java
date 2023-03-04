185877f2b37b43b572ddbd6e29e974e9de95e1ff
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-03-05 00:01:16 +0330

    Fixed Error HttpErrorResponse

--- src/main/java/com/kazemi/online_shopping/dao/ProductCategoryRepository.java
+++ src/main/java/com/kazemi/online_shopping/dao/ProductCategoryRepository.java
@@ -3,11 +3,13 @@ package com.kazemi.online_shopping.dao;
 import com.kazemi.online_shopping.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author fh.kazemi
  **/
 //collectionResourceRel: Name of JSON entry - path: /product-category
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+@CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/kazemi/online_shopping/dao/ProductRepository.java
+++ src/main/java/com/kazemi/online_shopping/dao/ProductRepository.java
@@ -2,9 +2,12 @@ package com.kazemi.online_shopping.dao;
 
 import com.kazemi.online_shopping.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author fh.kazemi
  **/
+
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
