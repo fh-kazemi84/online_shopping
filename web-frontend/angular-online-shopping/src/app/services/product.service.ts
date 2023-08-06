d2548978fbb4262e02c158f5a2384f903628c8f2
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-08-06 20:10:18 +0200

    Add Routes for Search in Products

--- web-frontend/angular-online-shopping/src/app/services/product.service.ts
+++ web-frontend/angular-online-shopping/src/app/services/product.service.ts
@@ -4,20 +4,20 @@ import {Product} from '../common/product';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  private baseUrl = "http://localhost:8080/api/products";
+  constructor(private httpClient: HttpClient) { }
 
-  constructor(private httpClient: HttpClient) {
-  }
+  getProductList(theCategoryId: number): Observable<Product[]> {
 
+    // need to build URL based on category id
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-  getProductList(): Observable<Product[]> {
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
