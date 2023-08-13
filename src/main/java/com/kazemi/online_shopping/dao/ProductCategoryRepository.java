34b9bae3508d9ba7720b903a6a77f8d74fef9bcc
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-08-13 20:11:24 +0200

    Update Repository Annotation.

--- src/main/java/com/kazemi/online_shopping/dao/ProductCategoryRepository.java
+++ src/main/java/com/kazemi/online_shopping/dao/ProductCategoryRepository.java
@@ -3,7 +3,6 @@ package com.kazemi.online_shopping.dao;
 import com.kazemi.online_shopping.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
  **/
 //collectionResourceRel: Name of JSON entry - path: /product-category
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
-@Repository
 @CrossOrigin("http://localhost:4200")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
