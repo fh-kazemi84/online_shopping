37d36021c5f9280f4efea626de7ac13540775bed
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-08-19 19:00:44 +0200

    Add Method for Pagination.

--- web-frontend/angular-online-shopping/src/app/components/product-list/product-list.component.ts
+++ web-frontend/angular-online-shopping/src/app/components/product-list/product-list.component.ts
@@ -9,9 +9,16 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products: Product[];
-  currentCategoryId: number;
-  searchMode: boolean;
+
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
+  searchMode: boolean = false;
+
+  // new properties for pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 5;
+  theTotalElements: number = 0;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -57,12 +64,34 @@ export class ProductListComponent {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
     }
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId},
+                 thePageNumber=${this.thePageNumber}`);
 
     // now get the products for the given category id
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.getProductListPaginate(
+      this.thePageNumber - 1, this.thePageSize, this.currentCategoryId)
+              .subscribe(this.processResult()
+              );
   }
+  processResult() {
+    return (data: any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    };
+  }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
 }
