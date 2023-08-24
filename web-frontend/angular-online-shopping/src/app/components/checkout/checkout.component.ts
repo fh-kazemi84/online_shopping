d984a6b2e7c9578a50787788cce73b106aa6e04b
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-08-24 20:32:18 +0200

    Add HandleMothsAndYear Method.

--- web-frontend/angular-online-shopping/src/app/components/checkout/checkout.component.ts
+++ web-frontend/angular-online-shopping/src/app/components/checkout/checkout.component.ts
@@ -88,4 +88,26 @@ export class CheckoutComponent implements OnInit {
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log("The email address is " + this.checkoutFormGroup.get('customer')?.value.email);
   }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+    const currentYear: number = new Date().getFullYear();
+    if (creditCardFormGroup != null) {
+      const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+      let startMonth: number;
+
+      if (currentYear === selectedYear) {
+        startMonth = new Date().getMonth() + 1;
+      } else {
+        startMonth = 1;
+      }
+
+      this.shopFormService.getCreditCardMonths(startMonth).subscribe(
+        data => {
+          console.log("Retrieved credit card months: " + JSON.stringify(data));
+          this.creditCardMonths = data;
+        }
+      );
+    }
+  }
 }
