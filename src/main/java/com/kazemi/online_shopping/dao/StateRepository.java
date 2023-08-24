78702dd56f7a559885d213779526836664418a5f
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-08-24 21:01:01 +0200

    Add FindByCountryCode Method.

--- src/main/java/com/kazemi/online_shopping/dao/StateRepository.java
+++ src/main/java/com/kazemi/online_shopping/dao/StateRepository.java
@@ -2,9 +2,12 @@ package com.kazemi.online_shopping.dao;
 
 import com.kazemi.online_shopping.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 /**
  * @author fh.kazemi
@@ -14,4 +17,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Integer> {
+
+    List<State> findByCountryCode(@Param("code") String code);
 }
