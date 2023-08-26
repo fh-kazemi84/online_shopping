54adbfecb8e9734738873c53f25cb3c7d8ce7417
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-08-27 00:16:37 +0200

    Fix bug for return null.

--- web-frontend/angular-online-shopping/src/app/validators/shop-validators.ts
+++ web-frontend/angular-online-shopping/src/app/validators/shop-validators.ts
@@ -2,7 +2,7 @@ import {FormControl, ValidationErrors} from '@angular/forms';
 
 export class ShopValidators {
   // whitespace validation
-  static notOnlyWhitespace(control: FormControl) : ValidationErrors {
+  static notOnlyWhitespace(control: FormControl) : ValidationErrors | null {
 
     // check if string only contains whitespace
     if ((control.value != null) && (control.value.trim().length === 0)) {
