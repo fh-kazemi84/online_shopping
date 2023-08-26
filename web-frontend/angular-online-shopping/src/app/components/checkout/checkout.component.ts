a6b5c12fbe9a49e572124692067e827eaa90a5f3
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-08-27 01:03:17 +0200

    Add validators for CreditCard.

--- web-frontend/angular-online-shopping/src/app/components/checkout/checkout.component.ts
+++ web-frontend/angular-online-shopping/src/app/components/checkout/checkout.component.ts
@@ -78,10 +78,18 @@ export class CheckoutComponent implements OnInit {
                                         ,ShopValidators.notOnlyWhitespace])
       }),
       creditCard: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
+        cardType: new FormControl('', [Validators.required]),
+
+        nameOnCard:  new FormControl('', [Validators.required
+                                            ,Validators.minLength(2)
+                                            ,ShopValidators.notOnlyWhitespace]),
+
+        cardNumber: new FormControl('', [Validators.required
+                                            ,Validators.pattern('[0-9]{16}')]),
+
+        securityCode: new FormControl('', [Validators.required
+                                              ,Validators.pattern('[0-9]{3}')]),
+
         expirationMonth: [''],
         expirationYear: ['']
       })
@@ -128,6 +136,11 @@ export class CheckoutComponent implements OnInit {
   get billingAddressCountry() { return this.checkoutFormGroup.get('billingAddress.country'); }
   get billingAddressState() { return this.checkoutFormGroup.get('billingAddress.state'); }
   get billingAddressZipCode() { return this.checkoutFormGroup.get('billingAddress.zipCode'); }
+
+  get creditCardType() { return this.checkoutFormGroup.get('creditCard.cardType'); }
+  get creditCardNameOnCard() { return this.checkoutFormGroup.get('creditCard.nameOnCard'); }
+  get creditCardNumber() { return this.checkoutFormGroup.get('creditCard.cardNumber'); }
+  get creditCardSecurityCode() { return this.checkoutFormGroup.get('creditCard.securityCode'); }
 
   copyShippingAddressToBillingAddress(event: Event) {
 
