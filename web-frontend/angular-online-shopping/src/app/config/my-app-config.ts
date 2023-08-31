f0e2675d750c3b09659a6341d2d13aa035d91267
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-08-31 22:07:36 +0200

    Create default config for okta.com

---
+++ web-frontend/angular-online-shopping/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oab24xki88AynIJy5d7',
+    issuer: 'https://dev-13218525.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
