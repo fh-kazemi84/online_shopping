2ddf641a5788cb846f9f37b6b18c0b177d31cdf3
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-04 00:08:29 +0200

    Update FormControl of email.

--- web-frontend/angular-online-shopping/src/app/components/checkout/checkout.component.ts
+++ web-frontend/angular-online-shopping/src/app/components/checkout/checkout.component.ts
@@ -31,6 +31,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage: Storage = sessionStorage;
+
   constructor(private formBuilder: FormBuilder,
               private shopFormService: ShopFormService,
               private cartService: CartService,
@@ -40,6 +42,9 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
 
     this.reviewCartDetails();
+
+    // read the user's email address from browser storage
+    const theEmail = JSON.parse(<string>this.storage.getItem('userEmail'));
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
@@ -51,7 +56,7 @@ export class CheckoutComponent implements OnInit {
                                           ,Validators.minLength(2)
                                           ,ShopValidators.notOnlyWhitespace]),
 
-        email: new FormControl('',[Validators.required
+        email: new FormControl(theEmail,[Validators.required
                                       ,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
