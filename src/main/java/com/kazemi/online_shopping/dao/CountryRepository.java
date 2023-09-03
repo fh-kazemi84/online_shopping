e43d0d25418b463e9e630833ac2f1e46f3e00c2a
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-03 12:49:57 +0200

    Remove CrossOrigins annotation.

--- src/main/java/com/kazemi/online_shopping/dao/CountryRepository.java
+++ src/main/java/com/kazemi/online_shopping/dao/CountryRepository.java
@@ -3,13 +3,11 @@ package com.kazemi.online_shopping.dao;
 import com.kazemi.online_shopping.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author fh.kazemi
  **/
 
-@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
 public interface CountryRepository extends JpaRepository<Country, Integer> {
 }
