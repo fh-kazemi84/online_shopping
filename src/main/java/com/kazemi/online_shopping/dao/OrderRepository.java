15ef72ab95d745def381fc5296ae58a67ec17b04
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-03 21:01:15 +0200

    Create and update interface.

---
+++ src/main/java/com/kazemi/online_shopping/dao/OrderRepository.java
@@ -0,0 +1,21 @@
+package com.kazemi.online_shopping.dao;
+
+import com.kazemi.online_shopping.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * @author fh.kazemi
+ **/
+
+@RepositoryRestResource
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Page<Order> findByCustomerEmailOrderByDateCreatedDesc(
+            @Param("email") String email,
+            Pageable pageable);
+
+}
